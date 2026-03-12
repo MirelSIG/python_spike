16d7e61727d4f1510c20f9923057a5a70d264c2d
LuisAlfonso107 <luisalfonsogarcialagos9@gmail.com>
2026-03-12 08:49:37 +0100

    ejemplos

--- LuisPY/objetos-python/objetos_basicos.py
+++ LuisPY/objetos-python/objetos_basicos.py
@@ -4,11 +4,11 @@ class Robot:
 
     # 1. EL PREPARADOR (init)
     def __init__(self, nombre, version):
-        self.nombre = nombre    # Atributo de instancia
-        self.version = version  # Atributo de instancia
+        self.nombre = nombre    
+        self.version = version  
         Robot.total_robots += 1
 
-    # 2. EL LENTE (str) - Para que print() funcione bonito
+    # 2. Para que print() funcione 
     def __str__(self):
         return f" Robot: {self.nombre} (v{self.version})"
 
@@ -33,3 +33,61 @@ bot2 = Robot("C3PO", 2.1)
 print(bot1)                # <--- Aquí usas el "LENTE" (__str__)
 bot1.saludar()             # <--- Método NORMAL
 Robot.cuantas_unidades()   # <--- Método de CLASE (el administrador)
+
+robot = {"nombre": "R2-D2", "version": 1.2}
+
+
+
+#-------------------------------------------------------------
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+p = Persona("Ana", 25)
+
+#-------------------------------------------------------------
+
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+        def __str__(self):
+            return f"Persona llamada {self.nombre}"
+
+p = Persona("Ana")
+print(p)
+
+#-------------------------------------------------------------
+
+class Coche:
+    def arrancar(self):
+        print("El coche está arrancando")
+
+mi_coche = Coche()
+mi_coche.arrancar()
+
+
+#-------------------------------------------------------------
+class Persona:
+    especie = "Humano"
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    @classmethod
+    def crear_sin_nombre(cls):
+        return cls("Desconocido")
+
+p = Persona.crear_sin_nombre()
+#-------------------------------------------------------------
+
+class Matematica:
+
+    @staticmethod
+    def sumar(a, b):
+        return a + b
+
+resultado = Matematica.sumar(5, 3)
+print(resultado)
